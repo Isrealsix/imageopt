1d908c4d1bab2e97ce9adafce8cea1ded9f72280
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-10-11 02:25:23 +0700

    environment platform setup

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 const { app, BrowserWindow} = require('electron')
 
+// Set env
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env === 'development' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
 let mainWindow
 function createMainWindow () {
   mainWindow = new BrowserWindow({
     title: 'Image optimizer',
     width: 1280,
     height: 800,
-    icon: './asswts/icons/Icon_256x256.png'
+    icon: './asswts/icons/Icon_256x256.png',
+    resizable: isDev ? true : false
   })
   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 }
+
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+})
 
 app.on('ready', createMainWindow)
