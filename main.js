5dd11fd34a233e85d862e96e0b52ece9bd2cf322
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-10-12 17:43:04 +0700

    added menu roles

--- main.js
+++ main.js
@@ -43,13 +43,27 @@ app.on('ready', () => {
 const menu = [
   ...(isMac ? [{role: 'appMenu'}] : []),
   {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+W',
-        click: () => app.quit()
-      }
-    ]
-  }
+    role: 'fileMenu'
+  },
+  ...(isDev? [
+    {
+      label: 'Developer',
+      submenu: [
+        { role: 'reload'},
+        { role: 'forcereload'},
+        { type: 'separator'},
+        { role: 'toggledevtools'}
+      ]
+    }
+  ] : [])
+  // {
+  //   label: 'File',
+  //   submenu: [
+  //     {
+  //       label: 'Quit',
+  //       accelerator: 'CmdOrCtrl+W',
+  //       click: () => app.quit()
+  //     }
+  //   ]
+  // }
 ]
